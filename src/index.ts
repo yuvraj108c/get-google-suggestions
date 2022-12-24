023a480f5f70805948dad7550a7912150f526f82
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2022-12-24 16:18:35 +0400

    implement getGoogleSuggestions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import axios from 'axios';
+
+async function getGoogleSuggestions(query: string): Promise<string[]> {
+  if (query.length === 0) return Promise.resolve([]);
+
+  const response = await axios.get(
+    `https://suggestqueries.google.com/complete/search?client=chrome-omni&q=${query}`
+  );
+  const suggestions = await response.data;
+  return suggestions[1];
+}
+
+// export default getGoogleSuggestions;
+module.exports = getGoogleSuggestions;
